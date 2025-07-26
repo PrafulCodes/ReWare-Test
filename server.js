1415b7dea811630816bca22f0cf12d1b25cbfd21
Praful Mohite <codewithpraful2006@gmail.com>
2025-07-27 02:46:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -131,4 +131,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 App is running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("🚀 ReWear backend is live");
+});
 
